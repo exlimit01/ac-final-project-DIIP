07b844f0b4bbda791e407b650f04d3ff0010b68e
Owen Ou <matsu727@me.com>
2016-07-12 22:44:25 +0800

    Add Model message

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160712025008) do
+ActiveRecord::Schema.define(version: 20160712143609) do
 
   create_table "answers", force: :cascade do |t|
     t.text     "content"
@@ -69,6 +69,12 @@ ActiveRecord::Schema.define(version: 20160712025008) do
 
   create_table "locations", force: :cascade do |t|
     t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
