4b6b32697e29aefab59af4074d5e8a3a2053888d
Owen Ou <matsu727@me.com>
2016-07-12 23:09:32 +0800

    Add controller rooms

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
+  
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-
 
   scope :path => '/api/v1/', :module => "api_v1", :as => 'v1', :defaults => { :format => :json } do
     resources :missions
   end
 
   root 'welcome#hello_world'
+
+  get 'rooms/show'
 
 end
