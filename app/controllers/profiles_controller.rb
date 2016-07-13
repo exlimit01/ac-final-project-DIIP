1234dbaeef8612085f4b7bb58114d7b77ba58ac1
YaoShangTseng <yakushou730@gmail.com>
2016-07-13 21:15:40 +0800

    generate profiles controller

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,40 @@
+class ProfilesController < ApplicationController
+
+  # GET /profiles
+  def index
+
+  end
+
+  # GET /profiles/:id
+  def show
+
+  end
+
+  # GET /profiles/new
+  def new
+
+  end
+
+  #POST /profiles
+  def create
+
+  end
+
+  #GET /profiles/:id/edit
+  def edit
+
+  end
+
+  #PATCH /profiles/:id
+  def update
+
+  end
+
+  #DELETE /profiles/:id
+  def destroy
+
+  end
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  resources :profiles do
+
+  end
 
 
   scope :path => '/api/v1/', :module => "api_v1", :as => 'v1', :defaults => { :format => :json } do
