2e1321fe5dc9c363824a430fbd0da63cb4e8a417
YaoShangTseng <yakushou730@gmail.com>
2016-07-14 20:05:08 +0800

    apply basic profile CRUD

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,8 +1,10 @@
 class ProfilesController < ApplicationController
 
+  before_action :set_profile, :only => [:show, :edit, :update, :destroy]
+
   # GET /profiles
   def index
-
+    @profiles = Profile.all
   end
 
   # GET /profiles/:id
@@ -12,12 +14,20 @@ class ProfilesController < ApplicationController
 
   # GET /profiles/new
   def new
-
+    @profile = Profile.new
   end
 
   #POST /profiles
   def create
+    @profile = Profile.new(profile_params)
+    @profile.save
 
+    if params[:photos]
+      params[:photos].each do |photo|
+        @profile.photos.create(:photo => photo)
+      end
+    end
+    redirect_to profiles_path
   end
 
   #GET /profiles/:id/edit
@@ -28,13 +38,44 @@ class ProfilesController < ApplicationController
   #PATCH /profiles/:id
   def update
 
+    if params[:_remove_avatar] == "1"
+      @profile.avatar = nil
+    end
+
+    if params[:_remove_photos] == "1"
+      @profile.photos.destroy_all
+    end
+
+    if @profile.update(profile_params)
+
+      if params[:photos]
+        params[:photos].each do |photo|
+          @profile.photos.create(:photo => photo)
+        end
+      end
+
+      redirect_to profiles_path
+
+    end
+
   end
 
   #DELETE /profiles/:id
   def destroy
-
+    @profile.destroy
+    redirect_to profiles_path
   end
 
+  private
+
+  def set_profile
+    @profile = Profile.find(params[:id])
+  end
+
+  def profile_params
+    params.require(:profile).permit(:nickname, :age, :avatar, :photos, :description, :facebook_link,
+      :facebook_access_level ,:line_account, :line_access_level, :wechat_account, :wechat_access_level, :location_id)
+  end
 
 
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -8,4 +8,10 @@ class Profile < ApplicationRecord
   has_many :professions, through: :profession_tags
 
   belongs_to :location
+
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+
+  has_many :photos, dependent: :destroy
+
 end
