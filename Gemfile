e066742002f0fea766ef76baa20c6954833ca004
YaoShangTseng <yakushou730@gmail.com>
2016-07-15 16:36:04 +0800

    change database from sqlite to mysql

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.0'
 # Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+#gem 'sqlite3'
 # Use Puma as the app server
 gem 'puma', '~> 3.0'
 # Use SCSS for stylesheets
@@ -34,6 +34,7 @@ gem 'bootstrap-sass'
 gem 'devise'
 gem 'omniauth-facebook'
 gem 'paperclip'
+gem 'mysql2'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/schema.rb
+++ db/schema.rb
@@ -12,76 +12,76 @@
 
 ActiveRecord::Schema.define(version: 20160714075531) do
 
-  create_table "answers", force: :cascade do |t|
-    t.text     "content"
-    t.integer  "likes",       default: 0
+  create_table "answers", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.text     "content",     limit: 65535
+    t.integer  "likes",                     default: 0
     t.integer  "user_id"
     t.integer  "question_id"
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
-    t.index ["question_id"], name: "index_answers_on_question_id"
-    t.index ["user_id"], name: "index_answers_on_user_id"
+    t.datetime "created_at",                            null: false
+    t.datetime "updated_at",                            null: false
+    t.index ["question_id"], name: "index_answers_on_question_id", using: :btree
+    t.index ["user_id"], name: "index_answers_on_user_id", using: :btree
   end
 
-  create_table "feedbacks", force: :cascade do |t|
-    t.text     "content"
+  create_table "feedbacks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.text     "content",    limit: 65535
     t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_feedbacks_on_user_id"
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+    t.index ["user_id"], name: "index_feedbacks_on_user_id", using: :btree
   end
 
-  create_table "friendships", force: :cascade do |t|
+  create_table "friendships", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id"
     t.integer  "friend_id"
     t.integer  "love_level", default: 0
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
-    t.index ["friend_id"], name: "index_friendships_on_friend_id"
-    t.index ["user_id"], name: "index_friendships_on_user_id"
+    t.index ["friend_id"], name: "index_friendships_on_friend_id", using: :btree
+    t.index ["user_id"], name: "index_friendships_on_user_id", using: :btree
   end
 
-  create_table "hobbies", force: :cascade do |t|
+  create_table "hobbies", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "hobby_tags", force: :cascade do |t|
+  create_table "hobby_tags", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "profile_id"
     t.integer  "hobby_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["hobby_id"], name: "index_hobby_tags_on_hobby_id"
-    t.index ["profile_id"], name: "index_hobby_tags_on_profile_id"
+    t.index ["hobby_id"], name: "index_hobby_tags_on_hobby_id", using: :btree
+    t.index ["profile_id"], name: "index_hobby_tags_on_profile_id", using: :btree
   end
 
-  create_table "interactions", force: :cascade do |t|
+  create_table "interactions", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "mission_id"
     t.integer  "friendship_id"
-    t.integer  "score",         default: 0
-    t.text     "comment"
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
-    t.index ["friendship_id"], name: "index_interactions_on_friendship_id"
-    t.index ["mission_id"], name: "index_interactions_on_mission_id"
+    t.integer  "score",                       default: 0
+    t.text     "comment",       limit: 65535
+    t.datetime "created_at",                              null: false
+    t.datetime "updated_at",                              null: false
+    t.index ["friendship_id"], name: "index_interactions_on_friendship_id", using: :btree
+    t.index ["mission_id"], name: "index_interactions_on_mission_id", using: :btree
   end
 
-  create_table "locations", force: :cascade do |t|
+  create_table "locations", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "missions", force: :cascade do |t|
-    t.text     "content"
-    t.integer  "unlock_level", default: 0
-    t.integer  "popular",      default: 0
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+  create_table "missions", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.text     "content",      limit: 65535
+    t.integer  "unlock_level",               default: 0
+    t.integer  "popular",                    default: 0
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
   end
 
-  create_table "photos", force: :cascade do |t|
+  create_table "photos", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "photo_file_name"
     t.string   "photo_content_type"
     t.integer  "photo_file_size"
@@ -89,64 +89,64 @@ ActiveRecord::Schema.define(version: 20160714075531) do
     t.integer  "profile_id"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
-    t.index ["profile_id"], name: "index_photos_on_profile_id"
+    t.index ["profile_id"], name: "index_photos_on_profile_id", using: :btree
   end
 
-  create_table "profession_tags", force: :cascade do |t|
+  create_table "profession_tags", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "profile_id"
     t.integer  "profession_id"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
-    t.index ["profession_id"], name: "index_profession_tags_on_profession_id"
-    t.index ["profile_id"], name: "index_profession_tags_on_profile_id"
+    t.index ["profession_id"], name: "index_profession_tags_on_profession_id", using: :btree
+    t.index ["profile_id"], name: "index_profession_tags_on_profile_id", using: :btree
   end
 
-  create_table "professions", force: :cascade do |t|
+  create_table "professions", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "profiles", force: :cascade do |t|
+  create_table "profiles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id"
-    t.datetime "created_at",                         null: false
-    t.datetime "updated_at",                         null: false
+    t.datetime "created_at",                                       null: false
+    t.datetime "updated_at",                                       null: false
     t.string   "nickname"
-    t.integer  "age",                   default: 18
-    t.text     "description"
+    t.integer  "age",                                 default: 18
+    t.text     "description",           limit: 65535
     t.string   "facebook_link"
-    t.integer  "facebook_access_level", default: 0
+    t.integer  "facebook_access_level",               default: 0
     t.string   "line_account"
-    t.integer  "line_access_level",     default: 0
+    t.integer  "line_access_level",                   default: 0
     t.string   "wechat_account"
-    t.integer  "wechat_access_level",   default: 0
+    t.integer  "wechat_access_level",                 default: 0
     t.integer  "location_id"
     t.string   "avatar_file_name"
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
-    t.index ["location_id"], name: "index_profiles_on_location_id"
-    t.index ["user_id"], name: "index_profiles_on_user_id"
+    t.index ["location_id"], name: "index_profiles_on_location_id", using: :btree
+    t.index ["user_id"], name: "index_profiles_on_user_id", using: :btree
   end
 
-  create_table "qa_customs", force: :cascade do |t|
-    t.text     "question"
-    t.text     "answer"
-    t.integer  "loves",         default: 0
+  create_table "qa_customs", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.text     "question",      limit: 65535
+    t.text     "answer",        limit: 65535
+    t.integer  "loves",                       default: 0
     t.integer  "friendship_id"
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
-    t.index ["friendship_id"], name: "index_qa_customs_on_friendship_id"
+    t.datetime "created_at",                              null: false
+    t.datetime "updated_at",                              null: false
+    t.index ["friendship_id"], name: "index_qa_customs_on_friendship_id", using: :btree
   end
 
-  create_table "questions", force: :cascade do |t|
-    t.text     "content"
-    t.integer  "unlock_level", default: 0
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+  create_table "questions", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.text     "content",      limit: 65535
+    t.integer  "unlock_level",               default: 0
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -161,9 +161,9 @@ ActiveRecord::Schema.define(version: 20160714075531) do
     t.datetime "updated_at",                          null: false
     t.string   "fb_uid"
     t.string   "fb_token"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["fb_uid"], name: "index_users_on_fb_uid"
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["fb_uid"], name: "index_users_on_fb_uid", using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
 end
