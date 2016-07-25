0fa54bfd6a8bf80cbeabe278e54f7638e408c43b
敏誠 李 <LeoLee@minchengde-MacBook-Air.local>
2016-07-25 13:45:05 +0800

    landing page v2

--- vendor/assets/javascripts/js/custom.js
+++ vendor/assets/javascripts/js/custom.js
@@ -1,5 +1,6 @@
 /* Write here your custom javascript codes */
 
+<<<<<<< 1f52bb610ac2f8912fc3dd746360ea00700defc0
 jQuery(document).ready(function() {
 
   function handleBootstrap() {
@@ -99,4 +100,32 @@ jQuery(document).ready(function() {
   handleFullscreen();
   handleValignMiddle();
   handleBootstrap();
+=======
+$(function(){
+
+  var note = $('#note');
+  var t_now = new Date().getTime();
+  var t_target = new Date(2016, 7, 5, 0, 0, 0);
+  var t_diff;
+
+  if(t_target > t_now){
+    t_diff = t_target - t_now;
+  }
+  var ts = t_now + t_diff;
+
+  $('#countdown').countdown({
+    timestamp : ts,
+    callback  : function(days, hours, minutes, seconds){
+
+      var message = "";
+
+      message += days + " 天" + ( days==1 ? '':'' ) + ", ";
+      message += hours + " 時" + ( hours==1 ? '':'' ) + ", ";
+      message += minutes + " 分" + ( minutes==1 ? '':'' ) + "  ";
+      message += seconds + " 秒" + ( seconds==1 ? '':'' ) + " <br />";
+
+      note.html(message);
+    }
+  });
+>>>>>>> landing page v2
 });
