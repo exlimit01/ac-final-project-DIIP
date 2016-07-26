aa1f4a4ba50cdf62cd06442cf7d1b1b9ea60f6cb
Owen Ou <matsu727@me.com>
2016-07-26 22:25:08 +0800

    fix profiles/new.html , _left_sidebar.html ,profile_pictures controller

--- app/controllers/profile_pictures_controller.rb
+++ app/controllers/profile_pictures_controller.rb
@@ -18,14 +18,18 @@ class ProfilePicturesController < ApplicationController
         user_id: current_user.id      
       ) 
       flash[:notice] = 'Avatar updated.'
-      redirect_to edit_profile_path(current_user.profile.id)   
+      redirect_to new_profile_path   
+    
     else
-
       @user_info.update(
         avatar_id: params[:avatar_id],
         user_id: current_user.id   
         )
+      if current_user.profile.nil?
+        redirect_to new_profile_path
+      else
       redirect_to edit_profile_path(current_user.profile.id)  
+      end
     end               
   end
 
