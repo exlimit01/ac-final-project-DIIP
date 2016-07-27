1647d8a5a84795a20f180e21fea571ae8c17f68e
YaoShangTseng <yakushou730@gmail.com>
2016-07-27 15:46:14 +0800

    add LikeAnswers action & fix show netwrok account bugs

--- app/controllers/like_answers_controller.rb
+++ app/controllers/like_answers_controller.rb
@@ -1,10 +1,18 @@
 class LikeAnswersController < ApplicationController
 
   def like
+
+    LikeAnswer.create!(friendship_id: params[:friendship_id], answer_id: params[:answer_id])
+    f = Friendship.find(params[:friendship_id])
+    f.love_level += 5
+    f.save
     redirect_to :back
   end
 
   def dislike
+byebug
+    #LikeAnswer.create!(friendship_id:, answer_id:)
+
     redirect_to :back
   end
 
