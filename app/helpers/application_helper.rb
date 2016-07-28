26190578f28d9f6d31fef2cb11287aa4556f8894
YaoShangTseng <yakushou730@gmail.com>
2016-07-28 12:15:04 +0800

    fix mission_remain? logic bug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,8 +5,10 @@ module ApplicationHelper
     number = 0
     friendships = Friendship.where(:user_id => current_user.id)
     friendships.each do |f|
-      unless f.interactions.last.v2_all_done?
-        number += 1
+      if f.interactions.count != 0
+        unless f.interactions.last.v2_all_done?
+          number += 1
+        end
       end
     end
     number
