b6c465a7cd6563fffc3b6c10b94727f4c50ffdc6
YaoShangTseng <yakushou730@gmail.com>
2016-07-28 11:12:46 +0800

    add showing mission open number

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  def check_mission_open_number
+
+    number = 0
+    friendships = Friendship.where(:user_id => current_user.id)
+    friendships.each do |f|
+      unless f.interactions.last.v2_all_done?
+        number += 1
+      end
+    end
+    number
+  end
+
 end
