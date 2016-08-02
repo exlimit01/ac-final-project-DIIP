8473489e9e3e7e251c872869f016040adc5ca1d7
YaoShangTseng <yakushou730@gmail.com>
2016-08-02 11:55:27 +0800

    fix change avatar bugs

--- app/controllers/profile_pictures_controller.rb
+++ app/controllers/profile_pictures_controller.rb
@@ -1,7 +1,7 @@
 class ProfilePicturesController < ApplicationController
-  
+
   def index
-    
+
   @avatars = Avatar.all
 
   end
@@ -9,28 +9,35 @@ class ProfilePicturesController < ApplicationController
 
 
   def create
-      
+
     @user_info = ProfilePicture.find_by(user_id: current_user.id)
 
     if @user_info.nil?
       ProfilePicture.create(
         avatar_id: params[:avatar_id],
-        user_id: current_user.id      
-      ) 
+        user_id: current_user.id
+      )
       flash[:notice] = 'Avatar updated.'
-      redirect_to new_profile_path   
-    
+
+      if current_user.profile
+        redirect_to new_profile_path
+      else
+        redirect_to edit_profile_path(current_user.profile.id)
+      end
+
+
+
     else
       @user_info.update(
         avatar_id: params[:avatar_id],
-        user_id: current_user.id   
+        user_id: current_user.id
         )
       if current_user.profile.nil?
         redirect_to new_profile_path
       else
-      redirect_to edit_profile_path(current_user.profile.id)  
+      redirect_to edit_profile_path(current_user.profile.id)
       end
-    end               
+    end
   end
 
 
